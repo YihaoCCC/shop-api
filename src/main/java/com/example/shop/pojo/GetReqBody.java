c64412e4ced433a19f278099591fe7e75312497c
su-ziyu <su669874427@163.com>
2021-12-12 20:49:19 +0800

    Changes by 柴渴

--- src/main/java/com/example/shop/pojo/GetReqBody.java
+++ src/main/java/com/example/shop/pojo/GetReqBody.java
@@ -4,18 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 /**
  * @author 苏聪杰
  * @Description
- * @date 2021/11/25
+ * @date 2021/12/1
  */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Collect {
+public class GetReqBody {
     private String userId;
+    private String usePoint;
+    private String addressId;
 
-    private List<Goods> goodsList;
 }
